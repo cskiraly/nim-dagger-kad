c259687cf13417d7dd8ff84be966273f9c702c51
Csaba Kiraly <csaba.kiraly@gmail.com>
2022-02-04 17:11:01 +0100

    add more test cases

--- tests/p2p/test_providers.nim
+++ tests/p2p/test_providers.nim
@@ -27,48 +27,88 @@ proc initDiscoveryNode(privKey: PrivateKey, address: Address,
 
   return node
 
-procSuite "Providers Tests":
+proc bootstrapNodes(nodecount: int, bootnodes: seq[ENode], rng = keys.newRng()) : Future[seq[DiscoveryProtocol]] {.async.} =
+
+    for i in 0..<nodecount:
+      let node = initDiscoveryNode(PrivateKey.random(rng[]), localAddress(20302 + i), bootnodes)
+      result.add(node)
+    
+    debug "---- STARTING BOOSTRAPS ---"
+
+    await allFutures(result.mapIt(it.bootstrap())) # this waits for bootstrap based on bootENode, which includes bonding with all its ping pongs
+
+proc bootstrapNetwork(nodecount: int, rng = keys.newRng()) : Future[seq[DiscoveryProtocol]] {.async.} =
   let
     bootNodeKey = PrivateKey.fromHex(
       "a2b50376a79b1a8c8a3296485572bdfbf54708bb46d3c25d73d2723aaaf6a617")[]
     bootNodeAddr = localAddress(20301)
     bootENode = ENode(pubkey: bootNodeKey.toPublicKey(), address: bootNodeAddr)
     bootNode = initDiscoveryNode(bootNodeKey, bootNodeAddr, @[]) # just a shortcut for new and open
-    rng = keys.newRng()
+
   waitFor bootNode.bootstrap()  # immediate, since no bootnodes are defined above
+  
+  result = await bootstrapNodes(nodecount - 1, @[bootENode], rng = rng)
+  result.insert(bootNode, 0)
 
-  proc bootstrapNodes(nodecount: int) : Future[seq[DiscoveryProtocol]] {.async.} =
 
-    for i in 0..<nodecount:
-      let bootnodes = @[bootENode]
-      let node = initDiscoveryNode(PrivateKey.random(rng[]), localAddress(20302 + i),
-        bootnodes)
-      result.add(node)
-    debug "---- STARTING BOOSTRAPS ---"
+suite "Providers Tests: node alone":
 
-    await allFutures(result.mapIt(it.bootstrap())) # this waits for bootstrap based on bootENode, which includes bonding with all its ping pongs
-    result.insert(bootNode, 0)
+  asyncTest "node alone":
+    let
+      rng = keys.newRng()
+      nodes = await bootstrapNetwork(nodecount=1)
 
-  asyncTest "Discover nodes UDP":
-    let nodes = await bootstrapNodes(nodecount=2)
-    #TODO: chek why it fails with 1
+    asyncTest "Node in isolation should store and retrieve":
 
-    info "---- STARTING LOOKUP ---"
+      info "---- STARTING LOOKUP ---"
 
-    let targetId = toNodeId(PrivateKey.random(rng[]).toPublicKey) 
-    let nodesFound = await nodes[0].kademlia.lookup(targetId)
-    info "nodes found: ", nodesFound
+      let targetId = toNodeId(PrivateKey.random(rng[]).toPublicKey) 
+      let nodesFound = await nodes[0].kademlia.lookup(targetId)
+      info "nodes found: ", nodesFound
 
-    info "---- ADDING PROVIDERS ---"
+      info "---- ADDING PROVIDERS ---"
 
-    let addedTo = await nodes[1].addProvider(targetId)
-    info "Provider added to: ", addedTo
+      let addedTo = await nodes[0].addProvider(targetId)
+      info "Provider added to: ", addedTo
 
-    info "---- STARTING PROVIDERS LOOKUP ---"
+      info "---- STARTING PROVIDERS LOOKUP ---"
 
-    let providers = await nodes[0].getProviders(targetId)
-    info "Providers:", providers
+      let providers = await nodes[0].getProviders(targetId)
+      info "Providers:", providers
 
-    info "---- STARTING CHECKS ---"
+      info "---- STARTING CHECKS ---"
 
-    check (providers.len > 0 and providers[0].id == nodes[1].thisNode.id)
+      check (providers.len > 0 and providers[0].id == nodes[0].thisNode.id)
+
+    nodes[0].close()
+    await sleepAsync(chronos.seconds(3))
+
+  asyncTest "Providers Tests: two nodes":
+    let
+      rng = keys.newRng()
+      nodes = await bootstrapNetwork(nodecount=2)
+
+    asyncTest "2 nodes, store and retieve":
+
+      info "---- STARTING LOOKUP ---"
+
+      let targetId = toNodeId(PrivateKey.random(rng[]).toPublicKey) 
+      let nodesFound = await nodes[0].kademlia.lookup(targetId)
+      info "nodes found: ", nodesFound
+
+      info "---- ADDING PROVIDERS ---"
+
+      let addedTo = await nodes[0].addProvider(targetId)
+      info "Provider added to: ", addedTo
+
+      info "---- STARTING PROVIDERS LOOKUP ---"
+
+      let providers = await nodes[0].getProviders(targetId)
+      info "Providers:", providers
+
+      info "---- STARTING CHECKS ---"
+
+      check (providers.len > 0 and providers[0].id == nodes[0].thisNode.id)
+
+    nodes[0].close
+    nodes[1].close
